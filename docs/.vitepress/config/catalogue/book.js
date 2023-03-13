cab3d6c84e4c8f0134e43a446f7faec4bce37618
fuyunlong <fuyunlong@delonix.group>
2023-03-13 09:45:34 +0800

    feat(books/): 添加书籍目录

--- docs/.vitepress/config/catalogue/book.js
+++ docs/.vitepress/config/catalogue/book.js
@@ -1,17 +1,23 @@
 const bookNav = {
-	text: '书籍',
-	items: [{ text: '书籍清单', link: '/pages/books/' }]
+  text: '书籍',
+  items: [
+    { text: '书籍清单', link: '/pages/books/' },
+    { text: '书籍笔记', link: '/pages/books/vuejs.md' },
+  ],
 }
 
 const bookSidebar = {
-	'/books/': [
-		{
-			text: '前端书籍',
-			collapsable: true,
-			sidebarDepth: 2,
-			items: [{ link: '/pages/books/js-pattern-excise.md', text: 'html介绍' }]
-		}
-	]
+  '/pages/books/': [
+    {
+      text: '前端书籍',
+      collapsable: true,
+      sidebarDepth: 2,
+      items: [
+        { link: '/pages/books/js-pattern-excise.md', text: '设计模式' },
+        { link: '/pages/books/vuejs.md', text: 'Vue js 设计与实现' },
+      ],
+    },
+  ],
 }
 
 export { bookNav, bookSidebar }
