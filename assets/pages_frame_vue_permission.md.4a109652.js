27d1a2d64c1f09ac151e285c23bed5d130c326b1
f5l5y5 <fuyefreedom@Gmail.com>
2023-04-03 21:37:30 +0000

    Deploying to gh-pages from  @ 70b90a423065710ba3dab7b16a40a10055cff952 🚀

---
+++ assets/pages_frame_vue_permission.md.4a109652.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./app.a53fec14.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"pages/frame/vue/permission.md","lastUpdated":1680471330000}'),s={name:"pages/frame/vue/permission.md"};function r(o,n,p,c,i,m){return a(),t("div")}const f=e(s,[["render",r]]);export{_ as __pageData,f as default};
